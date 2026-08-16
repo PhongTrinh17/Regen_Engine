891aceed3f320fe8792e11baac7d3ca1975de462
kyleslactaoen <kyleslactaoen@g.ucla.edu>
2026-08-16 16:02:52 -0700

    minor equation updates

--- regen_1d.m
+++ regen_1d.m
@@ -154,7 +154,7 @@ D_channel_base = D_gas + 2 * wall_thickness; % Engine diameters with added wall 
 D_t_base = D_t + 2 * wall_thickness; % Throat diameter with added wall thickness
 % Number of channels determined at throat
 circ_t_base = pi * (D_t_base); % Gas side diameter + wall thickness
-num_channel = 50;
+num_channel = floor(circ_t_base / (w_rib + min_tol));
 % Local circumferences across engine
 circ_local_base = pi * D_channel_base;
 w_channel = (circ_local_base - (num_channel * w_rib)) ./ num_channel; % variable, channel widths
@@ -215,29 +215,6 @@ prandtl_g_local = interp1(M_ref, prandtl_ref, M_local, 'linear', 'extrap');
 
 Taw = T_stag * ((1 + prandtl_g_local.^(1/3).*((gamma - 1) / 2) .* M_local.^2) ...
     ./ (1 + ((gamma - 1) / 2) .* M_local.^2));
-% Old code, doesn't work because interp1 need x values to be monotonically increasing, switch to using M_local
-%{ 
-AR_ref = [Rc^2/Rt^2, 1, eps];
-A_local = D_gas.^2 .* (pi/4);
-AR_local = A_local ./ At;
-cp_g_local = interp1(AR_ref, cp_g_ref, AR_local, 'linear', 'extrap');
-mu_g_local = interp1(AR_ref, mu_g_ref, AR_local, 'linear', 'extrap');
-k_g_local = interp1(AR_ref, k_g_ref, AR_local, 'linear', 'extrap');
-prandtl_g_local = interp1(AR_ref, prandtl_ref, AR_local, 'linear', 'extrap');
-% Local Mach from isentropic area-mach relation
-M_local = zeros(size(pos_i));
-for k = 1:length(pos_i)
-    if pos_i(k) < 0 % Chamber and Converging
-        M_local(k) = flowisentropic(gamma, AR_local(k), 'sub');
-    elseif pos_i(k) == 0
-        M_local(k) = 1.0;
-    else
-        M_local(k) = flowisentropic(gamma, AR_local(k), 'sup');
-    end
-end
-Taw = T_stag * ((1 + prandtl_g_local.^(1/3).*((gamma - 1) / 2) .* M_local.^2) ...
-    ./ (1 + ((gamma - 1) / 2) .* M_local.^2));
-%}
 
 %% Coolant Properties
 
@@ -370,6 +347,8 @@ T_sat_array = zeros(size(pos_i));
 h_c_f_array = zeros(size(pos_i));
 Re_array = zeros(size(pos_i));
 vel_c_array = zeros(size(pos_i));
+fin_eff_array = zeros(size(pos_i));
+q_error_array = zeros(size(pos_i));
 
 %% Main Loop
 P_guess = convpres(500, 'psi', 'Pa');
@@ -394,8 +373,8 @@ while abs(P_error) > tol_P % Pressure guess loop
         A_c_cs = cw*ch; % Cross sectional area of channel
         A_c_cs_tot = A_c_cs * num_channel;
         if (d ~= 1) % If not at final chamber station
-            A_c_cs_next = w_channel(d-1)*h_channel;
-            D_h_loc_next = (2*w_channel(d-1)*h_channel/(w_channel(d-1)+h_channel));
+            A_c_cs_next = w_channel(d-1)*ch;
+            D_h_loc_next = (2*w_channel(d-1)*ch/(w_channel(d-1)+ch));
         else % At final station
             A_c_cs_next = A_c_cs;
             D_h_loc_next = D_h_loc;
@@ -411,11 +390,8 @@ while abs(P_error) > tol_P % Pressure guess loop
         rho_c_v = get_rho_v(P_loc);
         surften = get_surften(P_loc);
         h_fg = get_h_fg(P_loc);
-
-        mu_g_local_us = mu_g_local(d) * 0.0559974; % Pa*s - (lb/in)*s
-        cp_g_local_us = cp_g_local(d) * 0.00023885; % J/(kg*K) - Btu/(lb*deg F)
        
-        vel_c = mdot_f / (A_c_cs_tot * rho_c); % coolant cs area instead
+        vel_c = mdot_f / (A_c_cs_tot * rho_c);
         Re = (rho_c * D_h_loc * vel_c) / mu_c;
         Re_array(d) = Re;
         vel_c_array(d) = vel_c;
@@ -463,10 +439,11 @@ while abs(P_error) > tol_P % Pressure guess loop
             iter_T = iter_T + 1;
             % Fin Efficiency
             k_w_loc = interp1(k_w_ref_temps, k_w_ref, T_hw_guess, 'linear', 'extrap');
-            fin_m = sqrt((2*h_c*(dx + w_rib))/(k_w_loc * dx * w_rib));
-            fin_eff = tanh(fin_m * h_channel) / (fin_m * h_channel);
-            h_c_f = h_c*(w_channel+2*fin_eff*h_channel)/(w_channel+w_rib);
+            fin_m = sqrt((2*h_c)/(k_w_loc * w_rib));
+            fin_eff = tanh(fin_m * ch) / (fin_m * ch);
+            h_c_f = h_c*(cw+2*fin_eff*ch)/(cw+w_rib); % Fin corrected h_c
             h_c_f_array(d) = h_c_f;
+            fin_eff_array(d) = fin_eff;
             % Gas convection HTC with Bartz
             sigma = 1 / ...
                 ((0.5 * (T_hw_guess/T_stag) * (1 + (gamma-1)/2 * M_local(d)^2) + 0.5)^0.68 *...
@@ -487,12 +464,12 @@ while abs(P_error) > tol_P % Pressure guess loop
             if (T_cw <= T_sat)
                 R_th = (1/(h_c_f*dx*(2*fin_eff*ch+cw)))+...
                     ((num_channel*log(1+2*wall_thickness/D_g_loc))/(2*pi*dx*k_w_loc));
-                q_3 = num_channel * (T_hw_guess-T_bulk) / R_th;
+                q_3 = ((T_hw_guess-T_bulk) / R_th);
             else % Nucleate
                 P_sat_T_cw = get_P_sat(T_cw);
                 h_nb = 0.00122*...
-                    (((k_c^0.79)*(cp_c^0.45)*(rho_c_l^0.49))/...
-                    ((surften^0.5)*(mu_c^0.29)*(h_fg^0.24)*(rho_c_v^0.24)))*...
+                    (((k_c^0.79)*(cp_c^0.45)*(rho_c_l^0.49)*9.81^0.25)/...
+                    ((surften^0.5)*(mu_c^0.25)*(h_fg^0.24)*(rho_c_v^0.24)))*...
                     ((T_cw-T_sat)^0.24)*...
                     (P_sat_T_cw - P_loc)^0.75;
                 S = 1/(1+(2.53*(10^-6))*(Re^1.17));
@@ -517,7 +494,6 @@ while abs(P_error) > tol_P % Pressure guess loop
 
             % Break
             if iter_T > 100
-                disp('T_hw failed to converge at station d =' + d);
                 break;
             end
         end
@@ -525,6 +501,7 @@ while abs(P_error) > tol_P % Pressure guess loop
         T_hw_array(d) = T_hw_guess;
         T_cw_array(d) = T_cw;
         h_g_array(d) = h_g;
+        q_error_array(d) = q_error;
 
         % Check CHF
         q_flux = q_1/(pi*(D_g_loc+wall_thickness*2)*dx);
@@ -700,4 +677,22 @@ plot(pos_i, vel_c_array, 'b', 'LineWidth', 2);
 title('Coolant Velocity')
 xlabel('Axial Position x (m)');
 ylabel('m/s');
+xline(0, 'k--', 'Throat', 'LabelVerticalAlignment', 'bottom', 'HandleVisibility', 'off');
+
+% Fin Eff
+figure('Name', 'Fin Efficiency', 'Color', 'w');
+hold on; grid on;
+plot(pos_i, fin_eff_array, 'b', 'LineWidth', 2);
+title('Fin Efficiency')
+xlabel('Axial Position x (m)');
+ylabel('Dimensionless');
+xline(0, 'k--', 'Throat', 'LabelVerticalAlignment', 'bottom', 'HandleVisibility', 'off');
+
+% Q error
+figure('Name', 'Q Error', 'Color', 'w');
+hold on; grid on;
+plot(pos_i, q_error_array, 'b', 'LineWidth', 2);
+title('Q Error')
+xlabel('Axial Position x (m)');
+ylabel('Dimensionless');
 xline(0, 'k--', 'Throat', 'LabelVerticalAlignment', 'bottom', 'HandleVisibility', 'off');
